4051edd40421ab34c79d04510682ab0d86fab015
j.Victor L o Diniz <71532868+victordinizzz@users.noreply.github.com>
2026-02-18 19:00:00 -0300

    colocado java para rodar

---
+++ backend/cronos/cronos/src/main/java/com/victor/cronos/CronosApplication.java
@@ -0,0 +1,13 @@
+package com.victor.cronos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CronosApplication.class, args);
+	}
+
+}
---
+++ backend/cronos/cronos/src/test/java/com/victor/cronos/CronosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.cronos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CronosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
